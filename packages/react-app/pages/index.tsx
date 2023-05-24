1da57ce1db4b83fe16e329b400fb144c9e32a40a
Dennis Kimathi <Kimathidennis38@gmail.com>
2023-05-25 00:17:30 +0300

    style: change modal backdrop color

--- packages/react-app/pages/index.tsx
+++ packages/react-app/pages/index.tsx
@@ -97,7 +97,7 @@ export default function Home() {
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl py-4 px-2 sm:py-6 sm:px-6 lg:max-w-7xl lg:px-2">
-        <div className="flex justify-between items-start mb-10">
+        <div className="flex flex-col sm:flex-row space-y-4  justify-between items-start sm:items-center mb-10">
           <h2 className="text-xl font-bold text-gray-900">Juakali Products</h2>
           <div className="flex space-x-4 items-center">
             <Search />
