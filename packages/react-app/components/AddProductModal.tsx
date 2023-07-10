10ea46591fe6a0f20b039b24f5e505fc44eadfff
Dennis Kimathi <Kimathidennis38@gmail.com>
2023-07-10 21:37:24 +0300

    chore: add test suite

--- packages/react-app/components/AddProductModal.tsx
+++ packages/react-app/components/AddProductModal.tsx
@@ -1,36 +1,230 @@
-import React from 'react';
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from '@/components/ui/alert-dialog';
-import ProductForm from './ProductForm';
-import { XCircleIcon } from '@heroicons/react/24/outline';
+// This component is used to add a product to the marketplace and show the user's cUSD balance
 
-export default function AddProductModal() {
+// Importing the dependencies
+import { useState } from 'react';
+// import ethers to convert the product price to wei
+import { ethers } from 'ethers';
+
+// Import the toast library to display notifications
+import { toast } from 'react-toastify';
+// Import the useDebounce hook to debounce the input fields
+import { useDebounce } from 'use-debounce';
+// Import our custom useContractSend hook to write a product to the marketplace contract
+import { useContractSend } from '@/hooks/contracts/useContractWrite';
+
+// The AddProductModal component is used to add a product to the marketplace
+const AddProductModal = () => {
+  // The visible state is used to toggle the modal
+  const [visible, setVisible] = useState(false);
+  // The following states are used to store the values of the form fields
+  const [productTitle, setProductTitle] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [category, setCategory] = useState('');
+  const [location, setLocation] = useState('');
+  const [price, setPrice] = useState<string | number>(0);
+  // The following states are used to store the debounced values of the form fields
+  const [debouncedProductTitle] = useDebounce(productTitle, 500);
+  const [debouncedImageUrl] = useDebounce(imageUrl, 500);
+  const [debouncedLocation] = useDebounce(location, 500);
+  const [debouncedPrice] = useDebounce(price, 500);
+  // The loading state is used to display a loading message
+  const [loading, setLoading] = useState('');
+
+  // Check if all the input fields are filled
+  const isComplete = productTitle && imageUrl && category && location && price;
+
+  // Clear the input fields after the product is added to the marketplace
+  const clearForm = () => {
+    setProductTitle('');
+    setImageUrl('');
+    setCategory('');
+    setLocation('');
+    setPrice(0);
+  };
+
+  // Convert the product price to wei
+  const priceInWei = ethers.utils.parseEther(debouncedPrice.toString());
+
+  // Use the useContractSend hook to use our writeProduct function on the marketplace contract and add a product to the marketplace
+  const { writeAsync: createProduct } = useContractSend('writeProduct', [
+    debouncedProductTitle,
+    debouncedImageUrl,
+    category,
+    debouncedLocation,
+    priceInWei,
+  ]);
+
+  // Define function that handles the creation of a product through the marketplace contract
+  const handleCreateProduct = async () => {
+    if (!createProduct) {
+      throw 'Failed to create product';
+    }
+    setLoading('Creating...');
+    if (!isComplete) throw new Error('Please fill all fields');
+    // Create the product by calling the writeProduct function on the marketplace contract
+    const purchaseTx = await createProduct();
+    setLoading('Waiting for confirmation...');
+    // Wait for the transaction to be mined
+    await purchaseTx.wait();
+    // Close the modal and clear the input fields after the product is added to the marketplace
+    setVisible(false);
+    clearForm();
+  };
+
+  // Define function that handles the creation of a product, if a user submits the product form
+  const addProduct = async (e: any) => {
+    e.preventDefault();
+    try {
+      // Display a notification while the product is being added to the marketplace
+      await toast.promise(handleCreateProduct(), {
+        pending: 'Creating product...',
+        success: 'Product created successfully',
+        error: 'Something went wrong. Try again.',
+      });
+      // Display an error message if something goes wrong
+    } catch (e: any) {
+      console.log({ e });
+      toast.error(e?.message || 'Something went wrong. Try again.');
+      // Clear the loading state after the product is added to the marketplace
+    } finally {
+      setLoading('');
+    }
+  };
+
+  // Define the JSX that will be rendered
   return (
-    <div className='font-sans'>
-      <AlertDialog>
-        <AlertDialogTrigger className='px-4 py-2 border border-button rounded-md  text-button hover:bg-button hover:text-white'>
-          List product
-        </AlertDialogTrigger>
+    <div className={'flex flex-row w-full justify-between'}>
+      <div>
+        {/* Add Product Button that opens the modal */}
+        <button
+          type='button'
+          onClick={() => setVisible(true)}
+          className='inline-block ml-4 px-6 py-2.5 bg-primary text-white font-medium text-md leading-tight rounded-lg shadow-md hover:bg-purple-900 hover:shadow-lg focus:bg-primary focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary active:shadow-lg transition duration-150 ease-in-out'
+          data-bs-toggle='modal'
+          data-bs-target='#exampleModalCenter'
+        >
+          Add Product
+        </button>
 
-        <AlertDialogContent>
-          <AlertDialogHeader className='flex flex-row items-center justify-between pb-2'>
-            <AlertDialogTitle>List a Juakali product</AlertDialogTitle>
-            <AlertDialogCancel className='border-none'>
-              <XCircleIcon className='h-6 w-6' />
-            </AlertDialogCancel>
-          </AlertDialogHeader>
-          <ProductForm />
-        </AlertDialogContent>
-      </AlertDialog>
+        {/* Modal */}
+        {visible && (
+          <div
+            className='fixed z-40 overflow-y-auto top-0 w-full left-0 '
+            id='modal'
+          >
+            {/* Form with input fields for the product, that triggers the addProduct function on submit */}
+            <form onSubmit={addProduct} className=''>
+              <div className='flex items-center justify-center min-height-100vh pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
+                <div className='fixed inset-0 transition-opacity'>
+                  <div className='absolute inset-0 bg-gray-900 opacity-75' />
+                </div>
+                <span className='hidden sm:inline-block sm:align-middle sm:h-screen'>
+                  &#8203;
+                </span>
+                <div
+                  className='inline-block align-center bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full'
+                  role='dialog'
+                  aria-modal='true'
+                  aria-labelledby='modal-headline'
+                >
+                  {/* Input fields for the product */}
+                  <div className='p-6 space-y-4'>
+                    <div className='w-full flex justify-between space-x-4'>
+                      <div>
+                        <label>Product Title</label>
+                        <input
+                          onChange={(e) => {
+                            setProductTitle(e.target.value);
+                          }}
+                          required
+                          type='text'
+                          className='w-full bg-gray-100 p-2 mt-2 mb-3'
+                        />
+                      </div>
+                      <div>
+                        <label>Product Image (URL)</label>
+                        <input
+                          onChange={(e) => {
+                            setImageUrl(e.target.value);
+                          }}
+                          required
+                          type='text'
+                          className='w-full bg-gray-100 p-2 mt-2 mb-3'
+                        />
+                      </div>
+                    </div>
+                    <div className='w-full flex justify-between space-x-4'>
+                      <div className='flex-col'>
+                        <label className='mb-3'>Product Category</label>
+
+                        <select
+                          value={category}
+                          onChange={(e) => {
+                            setCategory(e.target.value);
+                          }}
+                          className='w-full bg-gray-100 p-2 mt-2 mb-3'
+                        >
+                          <option defaultValue='Select category'>
+                            Select category
+                          </option>
+                          <option value='kitchen'>Kitchen</option>
+                          <option value='restaurant'>Restaurant</option>
+                          <option value='butchery'>Butchery</option>
+                          <option value='stainless'>Stainless</option>
+                        </select>
+                      </div>
+
+                      <div>
+                        <label>Location</label>
+                        <input
+                          onChange={(e) => {
+                            setLocation(e.target.value);
+                          }}
+                          required
+                          type='text'
+                          className='w-full bg-gray-100 p-2 mt-2 mb-3'
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <label>Product Price (cUSD)</label>
+                      <input
+                        onChange={(e) => {
+                          setPrice(e.target.value);
+                        }}
+                        required
+                        type='number'
+                        className='w-full bg-gray-100 p-2 mt-2 mb-3'
+                      />
+                    </div>
+                  </div>
+
+                  {/* Button to close the modal */}
+                  <div className='bg-gray-200 px-4 py-3 text-right'>
+                    <button
+                      type='button'
+                      className='py-2 px-4 bg-red-500 text-white rounded hover:bg-red-600 mr-2'
+                      onClick={() => setVisible(false)}
+                    >
+                      <i className='fas fa-times'></i> Cancel
+                    </button>
+                    {/* Button to add the product to the marketplace */}
+                    <button
+                      type='submit'
+                      disabled={!!loading || !isComplete || !createProduct}
+                      className='py-2 px-4 bg-primary text-white rounded hover:bg-purple-900 mr-2'
+                    >
+                      {loading ? loading : 'Create'}
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        )}
+      </div>
     </div>
   );
-}
+};
+
+export default AddProductModal;
--- packages/react-app/components/CheckoutModal.tsx
+++ packages/react-app/components/CheckoutModal.tsx
@@ -1,18 +1,7 @@
 // import { useMarketPlace } from "@/context/MarketPlaceContext";
 import { useContext, useState } from 'react';
 import { ShoppingCart } from 'lucide-react';
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from '@/components/ui/alert-dialog';
-import { XCircleIcon } from '@heroicons/react/24/outline';
+
 import { ShoppingCartContext } from '@/context/ShoppingCartContext';
 import { ethers } from 'ethers';
 import Image from 'next/image';
@@ -26,7 +15,9 @@ import { convertToEther } from '@/lib/convertToEther';
 import { useRouter } from 'next/navigation';
 
 export default function CheckoutModal() {
-  const {  clear, setError, setLoading } = useMarketPlace();
+  const { clear, setError, setLoading } = useMarketPlace();
+  // The visible state is used to toggle the modal
+  const [visible, setVisible] = useState(false);
 
   const { state, dispatch } = useContext(ShoppingCartContext);
 
@@ -35,16 +26,15 @@ export default function CheckoutModal() {
   const { cart } = state;
   let itemCount = 0;
   let totalPrice = 0;
-  let index = 0
+  let index = 0;
 
   for (const [key, value] of Object.entries(cart)) {
     itemCount = itemCount + cart[key].quantity;
   }
 
-   for (const [key, value] of Object.entries(cart)) {
-     index = cart[key].index;
-   }
-
+  for (const [key, value] of Object.entries(cart)) {
+    index = cart[key].index;
+  }
 
   for (const [key, value] of Object.entries(cart)) {
     totalPrice = totalPrice + cart[key].price * cart[key].quantity;
@@ -57,12 +47,13 @@ export default function CheckoutModal() {
   const { address } = useAccount();
 
   //Use the useContractSend hook to purchase the product with the id passed in, via the marketplace contract
-  const { writeAsync: purchase } = useContractSend("buyProduct", [index, itemCount]);
+  const { writeAsync: purchase } = useContractSend('buyProduct', [
+    index,
+    itemCount,
+  ]);
 
   //Use the useContractApprove hook to approve the spending of the total product's price, for the ERC20 cUSD contract
-  const { writeAsync: approve } = useContractApprove(
-    totalPrice.toString()
-  );
+  const { writeAsync: approve } = useContractApprove(totalPrice.toString());
   //Use the useConnectModal hook to trigger the wallet connect modal
   const { openConnectModal } = useConnectModal();
   //Format the product data that we read from the smart contract
@@ -70,13 +61,13 @@ export default function CheckoutModal() {
   //Define the handlePurchase function which handles the purchase interaction with the smart contract
   const handlePurchase = async () => {
     if (!approve || !purchase) {
-      throw "Failed to purchase this product";
+      throw 'Failed to purchase this product';
     }
     // Approve the spending of the product's price, for the ERC20 cUSD contract
     const approveTx = await approve();
     // Wait for the transaction to be mined, (1) is the number of confirmations we want to wait for
     await approveTx.wait(1);
-    setLoading("Purchasing...");
+    setLoading('Purchasing...');
     // Once the transaction is mined, purchase the product via our marketplace contract buyProduct function
     const res = await purchase();
     // Wait for the transaction to be mined
@@ -85,7 +76,7 @@ export default function CheckoutModal() {
 
   //Define the purchaseProduct function that is called when the user clicks the purchase button
   const purchaseProduct = async () => {
-    setLoading("Approving ...");
+    setLoading('Approving ...');
     clear();
 
     try {
@@ -96,111 +87,156 @@ export default function CheckoutModal() {
       }
       // If the user is connected, call the handlePurchase function and display a notification
       await toast.promise(handlePurchase(), {
-        pending: "Purchasing product...",
-        success: "Product purchased successfully",
-        error: "Failed to purchase product",
+        pending: 'Purchasing product...',
+        success: 'Product purchased successfully',
+        error: 'Failed to purchase product',
       });
+
       // If there is an error, display the error message
     } catch (e: any) {
       console.log({ e });
-      setError(e?.reason || e?.message || "Something went wrong. Try again.");
+      setError(e?.reason || e?.message || 'Something went wrong. Try again.');
       // Once the purchase is complete, clear the loading state
-    } 
+    } finally {
+	setLoading('');
+	  setVisible(false)
+
+	}
   };
 
   return (
     <>
-      <div className='font-sans'>
-        <AlertDialog>
-          <AlertDialogTrigger className=''>
-            <div className=' inset-0 flex items-center justify-center'>
-              <div className='relative flex justify-center items-center text-xl text-purple-900 rounded-full  h-12 w-12 border border-purple-900 hover:text-white hover:bg-purple-900'>
-                <ShoppingCart />
-                <div className='absolute  h-6 w-6 top-8 left-6 rounded-full bg-rose-700 flex justify-center items-center text-white text-sm'>
-                  {itemCount}
+      <div>
+        <div>
+          {/* Add Product Button that opens the modal */}
+          <button
+            type='button'
+            onClick={() => setVisible(true)}
+            data-bs-toggle='modal'
+            data-bs-target='#exampleModalCenter'
+            className=' inset-0 flex items-center justify-center'
+          >
+            <div className='relative flex justify-center items-center text-xl text-purple-900 rounded-full  h-12 w-12 border border-purple-900 hover:text-white hover:bg-purple-900'>
+              <ShoppingCart />
+              <div className='absolute  h-6 w-6 top-8 left-6 rounded-full bg-rose-700 flex justify-center items-center text-white text-sm'>
+                {itemCount}
+              </div>
+            </div>
+          </button>
+
+          {/* Modal */}
+          {visible && (
+            <div
+              className='fixed z-40 overflow-y-auto top-0 w-full left-0 '
+              id='modal'
+            >
+              {/* Form with input fields for the product, that triggers the addProduct function on submit */}
+              <div className='flex items-center justify-center min-height-100vh pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
+                <div className='fixed inset-0 transition-opacity'>
+                  <div className='absolute inset-0 bg-gray-900 opacity-75' />
+                </div>
+                <span className='hidden sm:inline-block sm:align-middle sm:h-screen'>
+                  &#8203;
+                </span>
+                <div
+                  className='inline-block align-center bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full'
+                  role='dialog'
+                  aria-modal='true'
+                  aria-labelledby='modal-headline'
+                >
+                  <div className='p-6'>
+                    {/* Input fields for the product */}
+                    {Object.entries(cart).map(([key, value]) => {
+                      return (
+                        <div
+                          key={cart[key].product_title}
+                          className='flex flex-col items-start gap-2'
+                        >
+                          <div className='flex text-black'>
+                            <img
+                              alt='cart item'
+                              src={cart[key].image_url}
+                              className='h-16 w-16 object-cover object-center'
+                            />
+                            <div className='pl-4'>
+                              <div className='flex flex-col'>
+                                <div className='font-bold'>
+                                  {cart[key].product_title}
+                                </div>
+                                <div className='flex items-center'>
+                                  <p className='pr-2 text-sm'>Quantity:</p>
+                                  <span className='text-md font-semibold'>
+                                    {cart[key].quantity}
+                                  </span>
+                                </div>
+                              </div>
+
+                              <div className='flex items-center'>
+                                <p className='pr-2 text-sm'>Price: </p>
+                                <div className='text-md font-semibold'>
+                                  {/* {cart[key].price} */}
+                                  {convertToEther(cart[key].price)}
+                                  <span className='pl-1'>cUSD</span>
+                                </div>
+                              </div>
+                            </div>{' '}
+                            <div>
+                              <button
+                                className='ml-1 border border-gray-900 rounded-full h-4 w-4 flex justify-center items-center font-medium cursor-pointer text-gray-900'
+                                onClick={() => {
+                                  dispatch({
+                                    type: 'REMOVE_FROM_CART',
+                                    payload: cart[key],
+                                  });
+                                }}
+                              >
+                                &times;
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })}
+
+                    <div className='font-bold py-4'>
+                      Total Price: {totalPriceFromWei} cUSD
+                    </div>
+                  </div>
+
+                  {/* Button to close the modal */}
+                  <div className='bg-primary/30 px-4 py-3 flex space-x-3 items-center justify-end'>
+                    <div>
+                      <button
+                        type='button'
+                        className='py-2 px-4 bg-red-500 text-white rounded hover:bg-red-600 mr-2'
+                        onClick={() => setVisible(false)}
+                      >
+                        <i className='fas fa-times'></i> Cancel
+                      </button>
+                    </div>
+                    <div>
+                      {/* Button to add the product to the marketplace */}
+
+                      {itemCount > 0 ? (
+                        <button
+                          className='inline-flex content-center place-items-center rounded-lg border text-white bg-button py-2 px-5 text-md font-medium hover:bg-[#8e24cc]'
+                          onClick={purchaseProduct}
+                        >
+                          Buy {itemCount} Product{itemCount > 1 && 's'}
+                        </button>
+                      ) : (
+                        <p>
+                          Your cart is empty. <br /> Add some products to your
+                          cart.
+                        </p>
+                      )}
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </AlertDialogTrigger>
-
-          <AlertDialogContent>
-            <AlertDialogHeader className='flex flex-row items-center justify-between pb-2'>
-              <AlertDialogTitle>Cart</AlertDialogTitle>
-              <AlertDialogCancel className='border-none'>
-                <XCircleIcon className='h-6 w-6' />
-              </AlertDialogCancel>
-            </AlertDialogHeader>
-            <div className='mt-2'>
-              {Object.entries(cart).map(([key, value]) => {
-                return (
-                  <div
-                    key={cart[key].product_title}
-                    className='flex flex-col items-start gap-2'
-                  >
-                    <div className='flex text-black'>
-                      <img
-                        alt='cart item'
-                        src={cart[key].image_url}
-                        className='h-16 w-16 object-cover object-center'
-                      />
-                      <div className='pl-4'>
-                        <div className='flex flex-col'>
-                          <div className='font-bold'>
-                            {cart[key].product_title}
-                          </div>
-                          <div className='flex items-center'>
-                            <p className='pr-2 text-sm'>Quantity:</p>
-                            <span className='text-md font-semibold'>
-                              {cart[key].quantity}
-                            </span>
-                          </div>
-                        </div>
-
-                        <div className='flex items-center'>
-                          <p className='pr-2 text-sm'>Price: </p>
-                          <div className='text-md font-semibold'>
-                            {/* {cart[key].price} */}
-                            {convertToEther(cart[key].price)}
-                            <span className='pl-1'>cUSD</span>
-                          </div>
-                        </div>
-                      </div>{' '}
-                      <div>
-                        <button
-                          className='ml-1 border border-gray-900 rounded-full h-4 w-4 flex justify-center items-center font-medium cursor-pointer text-gray-900'
-                          onClick={() => {
-                            dispatch({
-                              type: 'REMOVE_FROM_CART',
-                              payload: cart[key],
-                            });
-                          }}
-                        >
-                          &times;
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
-
-              <div className='font-bold py-4'>
-                Total Price: {totalPriceFromWei}
-              </div>
-              {itemCount > 0 ? (
-                <button
-                  className='inline-flex content-center place-items-center rounded-full border text-white bg-button py-2 px-5 text-md font-medium hover:bg-[#8e24cc]'
-                  onClick={purchaseProduct}
-                >
-                  Buy {itemCount} Product{itemCount > 1 && 's'}
-                </button>
-              ) : (
-                <p>
-                  Your cart is empty. <br /> Add some products to your cart.
-                </p>
-              )}
-            </div>
-          </AlertDialogContent>
-        </AlertDialog>
+          )}
+        </div>
       </div>
     </>
   );
